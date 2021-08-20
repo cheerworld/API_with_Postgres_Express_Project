6bc01c25682b4c05ea9bb4719c363296269087b0
Yuguo Zhao <cheerzhao77@gmail.com>
2021-08-20 15:20:13 -0500

    change orders table into 2 tables that create 1 to many relationship

--- migrations/sqls/20210812011539-orders-table-up.sql
+++ migrations/sqls/20210812011539-orders-table-up.sql
@@ -1,8 +1,6 @@
 /* Replace with your SQL commands */
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY,
-  quantity integer NOT NULL,
-  status VARCHAR(64) NOT NULL,
-  user_id bigint REFERENCES users(id) NOT NULL,
-  product_id bigint REFERENCES products(id) NOT NULL
+  status VARCHAR(64),
+  user_id bigint REFERENCES users(id)
 );
